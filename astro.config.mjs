c043c2a5d1759573aa2f2256f838062fc3f98100
arturoalbero <aag400@gmail.com>
2025-04-29 20:34:58 +0200

    cambiar apariencia y direccionamientos

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,16 +9,28 @@ const LIVE_URL = "https://arturoalbero.github.io";
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'Entornos de desarrollo',
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				github: 'https://www.boe.es/eli/es/rd/2023/05/29/405',
 			},
 			sidebar: [
 				{
-					label: 'Guides',
+					label: 'Unidades de programación',
 					items: [
 						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
+						{label: 'UP01', items:[]},
+						{label: 'UP02', items:[
+							{ label: 'Presentación', slug: 'up/up02/pildoras/00-presentacion-up2'},
+							{ label: 'El documento IEEE 830', slug: 'up/up02/pildoras/01-el-documento-ieee830'},
+							{ label: 'Casos de uso', slug: 'up/up02/pildoras/02-diagramas-cdu'},
+							{ label: 'Transición de estados', slug:'up/up02/pildoras/03-diagramas-dte'},
+							{ label: 'Actividad', slug: 'up/up02/pildoras/diagramas-act'},
+							{ label: 'Interacción', slug: 'up/up02/pildoras/05-diagramas-interac'}
+						]},
+						{label: 'UP03', items:[]},
+						{label: 'UP04', items:[]},
+						{label: 'UP05', items:[]},
+						{label: 'UP06', items:[]}
 					],
 				},
 				{
